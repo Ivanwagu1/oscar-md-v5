37008de58508dfe5598ea848563b9c440b7c80bf
Ivanwagu1 <115166614+Ivanwagu1@users.noreply.github.com>
2022-11-03 20:34:39 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 「 Suport Bot Dengan Cara donasi 」
 
-⫹⫺ Via Pulsa Indosat : 6285850539404
-⫹⫺ Via Dana : 6285850539404
+⫹⫺ Via Pulsa Indosat : 6285876830674
+⫹⫺ Via Dana : 085876830674
 ⫹⫺ Via Saweria :
 ⫹⫺ https://saweria.co/jarotoffc
 
